8b1502bf1d52fb76f1126f35dd834649d1c71ad7
rohit091998 <sharmarohitgwl@example.com>
2026-05-25 15:44:02 +0530

    added

--- dem.py
+++
@@ -1,5 +0,0 @@
-def my_fun():
-    print("hello friends, i am working as a devops engineer")
-    print("this line is newly added")
-    print("this is the third line")
-    print("new line added")
--- demo.py
+++
@@ -1,6 +0,0 @@
-def my_fun():
-    a=5
-    b=10
-    return d
-
-my_fun()
